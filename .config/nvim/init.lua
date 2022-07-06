b39ba00e5dc2dc0a37c5b7290f40e9c9e193cb64
Mohammad Elahi <moh.elahii@gmail.com>
2022-07-06 23:45:12 +0430

    dotfiles base sections initialization
    DONE
    ===
    - git config file is included
    - alacritty configs are added
    - i3 window manager base config is added
    - neovim config is added
    - stow ignore config is set

---
+++ .config/nvim/init.lua
@@ -0,0 +1,34 @@
+-- colorscheme settings
+require "user.colorscheme"
+
+-- default options changes
+require "user.options"
+
+-- autopairs configurations
+require "user.autopairs"
+
+-- commenter configuration
+require "user.comment"
+
+-- custom keymaps definition
+require "user.keymaps"
+
+-- plugins installatin
+require "user.plugins"
+
+-- Treesitter configurations
+require "user.treesitter"
+
+-- Telescope configuration
+require "user.telescope"
+
+-- LSP setup
+require "user.lsp"
+
+-- autocompletion configuration
+require "user.cmp"
+
+-- Lualine
+require "user.lualine"
+
+
---
+++ .config/nvim/lua/user/plugins.lua
@@ -0,0 +1,100 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+-- Install your plugins here
+return packer.startup(function(use)
+  -- My plugins here
+  use "wbthomason/packer.nvim" -- Have packer manage itself
+  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "windwp/nvim-autopairs" -- Autopairs plugin
+  use "numTostr/Comment.nvim" -- Commenter plugin
+  use 'JoosepAlviste/nvim-ts-context-commentstring' -- Commenter plugin for jsx
+
+  -- Colorschemes
+  use "tomasr/molokai" -- molokai colorscheme
+
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp" -- lsp source for nvim-cmp
+  use "hrsh7th/cmp-nvim-lua" -- nvim API for lua source for nvim-cmp
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+  -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- simple to use language server nvim-lsp-installer
+
+  -- Debugging
+  use "mfussenegger/nvim-dap"
+
+  -- conjure plugin for clojure
+  use 'Olical/conjure'
+
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+  use 'nvim-telescope/telescope-media-files.nvim'
+
+  -- Treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  -- Lualine
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
+
+
